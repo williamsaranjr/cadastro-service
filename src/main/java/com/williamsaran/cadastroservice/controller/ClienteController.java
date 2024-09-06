a99a0809cbe22a62b9acb1a103a6630d5811e38f
William Saran <williamsaranjr@gmail.com>
2024-09-06 14:34:09 -0300

    fix: corrigindo os MediaTypes gerados pelos endpoints

--- src/main/java/com/williamsaran/cadastroservice/controller/ClienteController.java
+++ src/main/java/com/williamsaran/cadastroservice/controller/ClienteController.java
@@ -4,6 +4,7 @@ import com.williamsaran.cadastroservice.model.dto.ClienteDTO;
 import com.williamsaran.cadastroservice.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,29 +13,32 @@ public class ClienteController {
     @Autowired
     private ClienteService service;
 
-    @GetMapping("{id}")
+    @GetMapping(value = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ClienteDTO encontrarPorId(@PathVariable Long id) {
         return service.encontrarPorId(id);
     }
 
-    @PostMapping
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public ClienteDTO criarConta(@RequestBody ClienteDTO dto) {
         return service.criarConta(dto);
     }
 
-    @PatchMapping
+    @PatchMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public ClienteDTO atualizarConta(@RequestBody ClienteDTO dto) {
         return service.atualizarConta(dto);
     }
 
-    @DeleteMapping("{id}")
+    @DeleteMapping(value = "{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deletarPorId(@PathVariable Long id) {
         service.deletarPorId(id);
     }
 
-    @PatchMapping("desativar/{id}")
+    @PatchMapping(value = "desativar/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public ClienteDTO desativarConta(@PathVariable Long id) {
         return service.desativarConta(id);
