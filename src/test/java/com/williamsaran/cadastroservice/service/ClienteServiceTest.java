8c751eddf9ab82312bdf23e6ce71e9e9d4cec3d4
William Saran <williamsaranjr@gmail.com>
2024-09-09 14:52:05 -0300

    tests: adicionando mais testes unitários de ClienteService

--- src/test/java/com/williamsaran/cadastroservice/service/ClienteServiceTest.java
+++ src/test/java/com/williamsaran/cadastroservice/service/ClienteServiceTest.java
@@ -1,6 +1,8 @@
 package com.williamsaran.cadastroservice.service;
 
 import com.williamsaran.cadastroservice.exception.ClienteNaoEncontradoException;
+import com.williamsaran.cadastroservice.exception.ContaDesativadaException;
+import com.williamsaran.cadastroservice.model.Cliente;
 import com.williamsaran.cadastroservice.repository.ClienteRepository;
 import com.williamsaran.cadastroservice.util.ClienteMapper;
 import com.williamsaran.cadastroservice.util.ClienteUtil;
@@ -26,9 +28,7 @@ public class ClienteServiceTest {
 
     @Test
     public void testEncontrarPorIdValido() {
-        Mockito.when(
-                repository.findById(1L)
-                ).thenReturn(Optional.of(ClienteUtil.criarClienteMock()));
+        buscarContaValida();
 
         var cliente = service.encontrarPorId(1L);
 
@@ -38,9 +38,7 @@ public class ClienteServiceTest {
 
     @Test
     public void testEncontrarPorIdInvalido() {
-        Mockito.when(
-                repository.findById(ArgumentMatchers.anyLong())
-        ).thenReturn(Optional.empty());
+        buscarContaInvalida();
 
         Assertions.assertThrows(
                 ClienteNaoEncontradoException.class,
@@ -50,13 +48,112 @@ public class ClienteServiceTest {
 
     @Test
     public void testCriarConta() {
-        Mockito.when(
-                repository.save(ArgumentMatchers.any())
-        ).thenReturn(ClienteUtil.criarClienteMock());
+        salvarContaValida();
 
         var entity = service.criarConta(ClienteUtil.criarDTOMock());
 
         Assertions.assertEquals(ClienteUtil.ID, entity.getId());
         Assertions.assertEquals(ClienteUtil.NOME, entity.getNome());
+    }
+
+    @Test
+    public void testAtualizarContaInvalida() {
+        buscarContaInvalida();
+
+        Assertions.assertThrows(
+                ClienteNaoEncontradoException.class,
+                () -> service.atualizarConta(ClienteUtil.criarDTOMock())
+        );
+    }
+
+    // TODO: implementar testes de atualização parcial de conta
+
+    @Test
+    public void testDeletarContaInvalida() {
+        buscarContaInvalida();
+
+        Assertions.assertThrows(
+                ClienteNaoEncontradoException.class,
+                () -> service.deletarPorId(1L)
+        );
+    }
+
+    @Test
+    public void testDeletarContaValida() {
+        Cliente cliente = ClienteUtil.criarClienteMock();
+
+        Mockito.when(
+                repository.findById(ArgumentMatchers.anyLong())
+        ).thenReturn(Optional.of(cliente));
+
+        service.deletarPorId(1L);
+
+        Mockito.verify(repository, Mockito.times(1)).delete(cliente);
+    }
+
+    @Test
+    public void testDesativarContaInvalida() {
+        buscarContaInvalida();
+
+        Assertions.assertThrows(
+                ClienteNaoEncontradoException.class,
+                () -> service.desativarConta(1L)
+        );
+    }
+
+    @Test
+    public void testDesativarContaDesativada() {
+        Cliente cliente = ClienteUtil.criarClienteMock();
+        cliente.setCorrentista(false);
+
+        Mockito.when(
+          repository.findById(ArgumentMatchers.anyLong())
+        ).thenReturn(Optional.of(cliente));
+
+        Assertions.assertThrows(
+                ContaDesativadaException.class,
+                () -> service.desativarConta(1L)
+        );
+    }
+
+    /**
+     * Instrui o Mock do {@link ClienteRepository} a retornar uma conta válida
+     * ao salvar uma nova conta.
+     */
+    private Cliente salvarContaValida() {
+        Cliente cliente = ClienteUtil.criarClienteMock();
+
+        Mockito.when(repository.save(ArgumentMatchers.any()))
+                .thenReturn(cliente);
+
+        return cliente;
+    }
+
+    /**
+     * Instrui o Mock do {@link ClienteRepository} a retornar uma conta válida
+     * ao realizar uma busca por ID.
+     */
+    private Cliente buscarContaValida() {
+        Cliente cliente = ClienteUtil.criarClienteMock();
+
+        Mockito.when(repository.findById(1L))
+                .thenReturn(Optional.of(cliente));
+
+        return cliente;
+    }
+
+    /**
+     * Instrui o Mock do {@link ClienteRepository} a retornar uma conta inválida
+     * ao realizar uma busca por ID.
+     *
+     * @return Um {@link Optional} de cliente vazio
+     */
+    private Optional<Cliente> buscarContaInvalida() {
+        Optional<Cliente> empty = Optional.empty();
+
+        Mockito.when(repository.findById(ArgumentMatchers.anyLong()))
+                .thenReturn(empty);
+
+        return empty;
     }
 }
