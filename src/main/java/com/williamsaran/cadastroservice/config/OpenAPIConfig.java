314f3e2dd9351f777f4cb6ccbd5f78c61c6cdcbd
William Saran <williamsaranjr@gmail.com>
2024-09-09 11:25:33 -0300

    docs: adicionando a documentação dos endpoints

---
+++ src/main/java/com/williamsaran/cadastroservice/config/OpenAPIConfig.java
@@ -0,0 +1,17 @@
+package com.williamsaran.cadastroservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Serviço de cadastro")
+                        .description("API para cadastro de clientes"));
+    }
+}
