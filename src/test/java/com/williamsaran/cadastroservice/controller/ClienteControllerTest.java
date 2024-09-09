26a7ca24ee3f43bb12b3bf58e52889fb8bfa6f32
William Saran <williamsaranjr@gmail.com>
2024-09-09 11:44:16 -0300

    feat: refatorando o teste de atualizar conta

--- src/test/java/com/williamsaran/cadastroservice/controller/ClienteControllerTest.java
+++ src/test/java/com/williamsaran/cadastroservice/controller/ClienteControllerTest.java
@@ -1,15 +1,20 @@
 package com.williamsaran.cadastroservice.controller;
 
+import com.google.gson.Gson;
 import com.williamsaran.cadastroservice.model.dto.ClienteDTO;
 import com.williamsaran.cadastroservice.service.ClienteService;
+import com.williamsaran.cadastroservice.util.ClienteMapper;
 import com.williamsaran.cadastroservice.util.ClienteMock;
 import lombok.NoArgsConstructor;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.*;
@@ -71,20 +76,23 @@ public class ClienteControllerTest {
                 .andExpect(status().isNoContent());
     }
 
-//    @Test
-//    public void testAtualizarConta() throws Exception {
-//        when(service.atualizarConta(dto)).thenReturn(dto);
-//
-//        mock.perform(patch("/clientes/")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(gson.toJson(dto))
-//                        .accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.id").value(ClienteMock.ID))
-//                .andExpect(jsonPath("$.nome").value(ClienteMock.NOME))
-//                .andExpect(jsonPath("$.telefone").value(ClienteMock.TELEFONE))
-//                .andExpect(jsonPath("$.correntista").value(ClienteMock.CORRENTISTA))
-//                .andExpect(jsonPath("$.scoreCredito").value(ClienteMock.SCORE_CREDITO))
-//                .andExpect(jsonPath("$.saldo").value(ClienteMock.SALDO));
-//    }
+    @Test
+    public void testAtualizarConta() throws Exception {
+        Gson gson = new Gson();
+
+        when(service.atualizarConta(ArgumentMatchers.any())).thenReturn(dto);
+
+        var response = mock.perform(patch("/clientes/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(gson.toJson(dto))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        var responseCliente = gson.fromJson(response, ClienteDTO.class);
+
+        Assertions.assertEquals(responseCliente.getId(), dto.getId());
+    }
 }
